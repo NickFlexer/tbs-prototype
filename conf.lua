5e726547b02e1f46e4e914722251877bc9ddb7d8
NickFlexer <nickkomarov.job@gmail.com>
2021-04-19 19:08:52 +0300

    init commit

---
+++ conf.lua
@@ -0,0 +1,10 @@
+---
+-- conf.lua
+
+
+function love.conf(t)
+    t.window.title = "TBS-prototype"
+
+    t.window.width = 1024
+    t.window.height = 768
+end
---
+++ main.lua
@@ -0,0 +1,20 @@
+---
+-- main.lua
+
+
+package.path = package.path .. ";lib/?/init.lua;lib/?.lua;src/?.lua"
+
+
+function love.load()
+
+end
+
+
+function love.update(dt)
+
+end
+
+
+function love.draw()
+
+end
