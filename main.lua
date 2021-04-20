68c34e23c8b68a3f2553a5b506ca71850a3e0519
NickFlexer <nickkomarov.job@gmail.com>
2021-04-20 22:26:17 +0300

    add lovetoys

--- main.lua
+++ main.lua
@@ -5,16 +5,32 @@
 package.path = package.path .. ";lib/?/init.lua;lib/?.lua;src/?.lua"
 
 
-function love.load()
+local lovetoys = require "lovetoys"
 
+local World = require "world.world"
+
+
+lovetoys.initialize({
+    globals = true,
+    debug = true
+})
+
+
+local world
+
+
+function love.load()
+    engine = Engine()
+
+    world = World()
 end
 
 
 function love.update(dt)
-
+    engine:update(dt, world)
 end
 
 
 function love.draw()
-
+    engine:draw()
 end
---
+++ src/world/world.lua
@@ -0,0 +1,14 @@
+---
+-- world.lua
+
+
+local class = require "middleclass"
+
+
+local World = class("World")
+
+function World:initialize(data)
+    -- body
+end
+
+return World
